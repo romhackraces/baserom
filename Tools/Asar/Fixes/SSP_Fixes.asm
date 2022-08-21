09e814da4f217caaa87c0b95eb0cad4a02ab2b3c
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2022-08-21 13:11:42 -0230

    Move Patches into Tools/Asar

--- Tools/Asar/Fixes/SSP_Fixes.asm
+++ Tools/Asar/Fixes/SSP_Fixes.asm
@@ -71,7 +71,7 @@ endif
 		!WalljumpNoteBlockFixPatch = 1
 	endif
 
-incsrc "../SSP_Defines.asm"
+incsrc "../../Defines/ScreenScrollingPipes.asm"
 
 	macro define_sprite_table(name, addr, addr_sa1)
 		if !sa1 == 0
