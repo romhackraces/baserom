a040a53fa2d4b65e2ceb98563268258c7d97ac7e
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-03-05 14:23:27 -0330

    update powerup filter with balloon things

--- Tools/GPS/blocks/remove_powerup.asm
+++ Tools/GPS/blocks/remove_powerup.asm
@@ -1,18 +1,25 @@
 ; Block that removes any powerup from Mario
 
-print "Block that removes any powerup or item from Mario as well as flight state"
+print "Block that makes Mario small, clears item box and removes balloon, flight, and slide state"
 
 db $42
 JMP Mario : JMP Mario : JMP Mario : JMP Return : JMP Return : JMP Return : JMP Return
 JMP Mario : JMP Mario : JMP Mario
 
 Mario:
-	STZ $19			; remove player's powerup
-	STZ $0DC2 		; remove player's item
-	STZ $1407		; remove cape flight
-	LDA $13ED		;\
-	AND #%01111111	;| remove slide state
-	STA $13ED		;/
-	
+    STZ $19                     ;> remove player's powerup
+    STZ $0DC2|!addr             ;> clear player's itembox
+    STZ $1407|!addr             ;> remove cape flight
+
+    LDA $13ED|!addr             ;\
+    AND #%01111111              ;| remove slide state
+    STA $13ED|!addr             ;/
+
+    LDA $13F3|!addr             ;\ check if in balloon
+    BEQ Return                  ;/ otherwise skip
+    LDA #$01 : STA $1891|!addr  ;> set p-balloon timer to 1 frame (can't do zero here)
+    STZ $13F3|!addr             ;> make sure not in balloon
+
 Return:
-	RTL
+    RTL
+
