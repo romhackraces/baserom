de8fdc7ecacfdb380dba9fe279cd206018760d12
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-03-29 18:00:14 -0230

    disable the deceleration options in player speed patch

--- Tools/Asar/Fixes/PlayerXSpeedFix.asm
+++ Tools/Asar/Fixes/PlayerXSpeedFix.asm
@@ -1,10 +1,10 @@
 ;Player X Speed Fix v3.0 by GreenHammerBro and tjb
-!FixOscillation = 1	;Fix the speed oscillation bug
-!FixDeceleration = 1	;Fix behavior where holding forward can slow you down if you're over the max speed
-			;  (requires !FixOscillation)
-!GroundDecel = 1 	;If 1, !FixDeceleration only applies in the air, not on the ground.
-			;  Setting this to 0 allows you to keep extra speed even on the ground, unlike vanilla.
-			;(the "No Jump" fix is always applied.)
+!FixOscillation = 1		;Fix the speed oscillation bug
+!FixDeceleration = 0	;Fix behavior where holding forward can slow you down if you're over the max speed
+						;  (requires !FixOscillation)
+!GroundDecel = 0 		;If 1, !FixDeceleration only applies in the air, not on the ground.
+						;  Setting this to 0 allows you to keep extra speed even on the ground, unlike vanilla.
+						;(the "No Jump" fix is always applied.)
 
 	!dp = $0000
 	!addr = $0000
@@ -136,7 +136,7 @@ if !FixOscillation != 0
 		LDY $72			;\if you're in the air, branch to the acceleration code
 		BNE .accel		;/
 		LDA $D43D,x		;> mario is on a slippery floor. load the slippery accel table instead
-	
+
 	.accel
 		CLC			;\accelerate mario
 		ADC $7A			;/
@@ -150,14 +150,14 @@ if !FixOscillation != 0
 		EOR $D535,y		;|> flip +/- bit based on direction
 		BPL .clamp		;/> branch if we just pushed it over the max speed
 		JML $00D7A4|!bank	;>jump to RTS
-		
+
 	.clamp
 		LDA $D535,y		;>clamp speed to exactly the maximum
 		STA $7B			;>store new speed
 	.resetfraction
 		STZ $7A			;>and reset fraction bits
 		JML $00D7A4|!bank	;>jump to RTS
-	
+
 	if !FixDeceleration
 		DecelTable:
 			; There are many different things that call this acceleration routine, and we only want to fix deceleration with some of them.
