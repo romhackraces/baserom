4253e78e7307012ad630128caee0373d7b6d53f0
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-07-15 17:05:02 -0230

    Added a couple optimization patches to prevent flicker

--- Tools/Asar/Tweaks.asm
+++ Tools/Asar/Tweaks.asm
@@ -1,9 +1,16 @@
+; SA-1 Check
 if read1($00FFD5) == $23
     sa1rom
     !sa1 = 1
 else
     lorom
     !sa1 = 0
+endif
+
+; EXLEVEL Check
+!EXLEVEL = 0
+if (((read1($0FF0B4)-'0')*100)+((read1($0FF0B4+2)-'0')*10)+(read1($0FF0B4+3)-'0')) > 253
+    !EXLEVEL = 1
 endif
 
 ;;;;;;;;;;;;;;;;;;
--- Tools/UberASMTool/other/global_code.asm
+++ Tools/UberASMTool/other/global_code.asm
@@ -5,6 +5,31 @@ load:
 init:
 	rts
 main:
+	!controller_read_optimization = 1 ; set to 0 to uninstall
+	pushpc
+	org $008650
+	ControllerUpdate:
+	if !controller_read_optimization
+		org $008243
+		BRA $01
+		org $0082F4
+		BRA $01
+		org $0086C6
+		RTL
+		pullpc
+		JSL ControllerUpdate|!bank
+
+	else
+		org $008243
+		JSR ControllerUpdate
+		org $0082F4
+		JSR ControllerUpdate
+		org $0086C6
+		RTS
+		pullpc
+	endif
+	assert read1($0086C1) != $5C, "Incompatible with optimized block change patch"
+
 	rts
 ;nmi:
 ;	rts
