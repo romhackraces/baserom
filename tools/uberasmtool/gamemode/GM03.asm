3491c72008a5f8c1a4223c2c296d9a93899c4cd5
underway <underwayu@gmail.com>
2023-09-04 15:23:19 +0200

    Move zeroing of custom object freeRAM to gamemode 10 init
    Gamemode 3 init would only run once at the start of the game and thus cause the flags to carry over between levels.

--- tools/uberasmtool/gamemode/GM03.asm
+++ tools/uberasmtool/gamemode/GM03.asm
@@ -1,4 +1,3 @@
 init:
-    jsl uberasm_objects_gm03_init
     jsl retry_load_title_init
     rtl
--- tools/uberasmtool/gamemode/GM10.asm
+++ tools/uberasmtool/gamemode/GM10.asm
@@ -1,3 +1,4 @@
 init:
+    jsl uberasm_objects_gm10_init
     jsl retry_level_transition_init
     rtl
