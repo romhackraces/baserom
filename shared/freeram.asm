38e76401770c6630ecdff102bb378cd88297a954
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-09-05 10:05:50 -0230

    Change skull raft fix freeram

--- shared/freeram.asm
+++ shared/freeram.asm
@@ -5,7 +5,7 @@ includeonce
 !capespin_direction_freeram = $1869|!addr ; 2 bytes
 !double_hit_fix_freeram = $1DFD|!addr ; 1 byte
 !screen_scrolling_pipes_freeram = $18C5|!addr ; 5 bytes
-!skull_raft_fix_freeram = $57 ; 1 byte
+!skull_raft_fix_freeram = $18E6|!addr ; 1 byte
 !sprite_scroll_fix_position_freeram = $0DC3|!addr ; 4 bytes
 !sprite_scroll_fix_displacement_freeram = $1487|!addr ; 4 bytes
 !triangles_fix_freeram = $14BE|!addr ; 1 byte
