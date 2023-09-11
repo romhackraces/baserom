d7d7772e9b47255e051f25333c0f6833d6385fb2
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-09-11 00:24:22 -0230

    Update ScreenScrollingPipes.asm

--- shared/defines/ScreenScrollingPipes.asm
+++ shared/defines/ScreenScrollingPipes.asm
@@ -1,40 +1,8 @@
 incsrc "../freeram.asm"
-;If you make changes here, make sure you apply the changes to all the copies of this define files
-;elsewhere by simply copying the folder containing this file.
 
-;If you're using notepad++, the tab size is 8 in case if you don't like the text vertical misalignment.
-
-;Places to put this define file to:
-;-In GPS's main directory (same where the exe is in, not in the sub-folder/subdirectory)
-;-In uberasm tool's main directory, same style as above.
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;SA1 detector:
-;Place this at the very top of gamemode_code.asm.
-;Do not change anything here unless you know what are you doing.
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-if defined("sa1") == 0
-    !dp = $0000
-    !addr = $0000
-    !sa1 = 0
-    !gsu = 0
-
-    if read1($00FFD6) == $15
-        sfxrom
-        !dp = $6000
-        !addr = !dp
-        !gsu = 1
-    elseif read1($00FFD5) == $23
-        sa1rom
-        !dp = $3000
-        !addr = $6000
-        !sa1 = 1
-    endif
-endif
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;uberasm code for GHB's screen scrolling pipes.
-;Do not insert this as blocks, paste this code in "gamemode_code.asm"
-;labeled "gamemode_14:" so blocks works in all levels.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Simplified defines for GHB's screen scrolling pipes.
+; see library/ScreenScrollingPipes.asm in UberASMTool's folder
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 !Freeram_SSP_PipeDir      = !screen_scrolling_pipes_freeram
