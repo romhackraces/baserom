6d6b814fab74b9e7f91057f069362476264cf313
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-11-25 15:21:41 -0330

    Update baserom_init.ps1

--- tools/init/baserom_init.ps1
+++ tools/init/baserom_init.ps1
@@ -221,7 +221,7 @@ while ($UserChoice -ne "4") {
                             # Create checkfile if all goes well
                             New-Item -Path "$Callisto_Dir.first_build_done" -ItemType File -Force | Out-Null
                             Set-ItemProperty -Path "$Callisto_Dir.first_build_done" -Name Attributes -Value ([System.IO.FileAttributes]::Hidden) | Out-Null
-                            Write-Host "First build completed successfully.`n`nYou can get started on your project by running Callisto from the 'buildtool' folder."
+                            Write-Host "First build completed successfully.`n`nYou will likely have seen Callisto report conflict(s), this is normal for the first build.`n`nYou can get started on your project by running Callisto from the 'buildtool' folder."
                         } else {
                             # Prompt users to run Callisto manually if there was an error
                             Write-Host "Baserom failed to build. Please run Callisto manually from the 'buildtool' folder, and perform a 'Rebuild' to see any errors."
