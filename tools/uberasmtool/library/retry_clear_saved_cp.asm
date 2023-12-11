24346a67ee563b2656d2401cb33054e9128484e2
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-12-10 21:31:12 -0330

    Update clear checkpoint feature's key combo

--- tools/uberasmtool/library/retry_clear_saved_cp.asm
+++ tools/uberasmtool/library/retry_clear_saved_cp.asm
@@ -5,26 +5,37 @@ incsrc "../retry_config/ram.asm"
 !b = 0		; B
 !y = 0		; Y
 !e = 1		; Select
-!t = 1		; Start
+!t = 0		; Start
 !U = 0		; Up
-!D = 1		; Down
+!D = 0		; Down
 !L = 0		; Left
 !R = 0		; Right
 
+!a = 0		; A
+!x = 0		; X
+!l = 1		; L
+!r = 1		; R
+
 ; interpret the above
-!Combo = (!b<<7)+(!y<<6)+(!e<<5)+(!t<<4)+(!U<<3)+(!D<<2)+(!L<<1)+!R
+!Combo1 = (!b<<7)+(!y<<6)+(!e<<5)+(!t<<4)+(!U<<3)+(!D<<2)+(!L<<1)+!R
+!Combo2 = (!a<<7)+(!x<<6)+(!l<<5)+(!r<<4)
 
 main:
-    lda $9D
-    ora $13D4|!addr
-    bne .return
-
-    ; Check for button combo
-    ldy $0DB3|!addr
+	lda $9D
+	ora $13D4|!addr
+	bne .return
+	ldy $0DB3|!addr
+	; byetUDLR check
     lda $0DA2|!addr,y
     ora $0DA6|!addr,y
-    and #!Combo
-    cmp #!Combo
+    and #!Combo1
+    cmp #!Combo1
+    bne .return
+	; axlr check
+    lda $0DA4|!addr,y
+    ora $0DA8|!addr,y
+    and #!Combo2
+    cmp #!Combo2
     beq clear_saved_checkpoints
 .return
     rtl
@@ -58,6 +69,11 @@ clear_saved_checkpoints:
     ; Restore X/Y/P
     plp : ply : plx
 
+
+; play clear sound effect
+.play_clear_sfx
+    lda #$0F : sta $1DF9|!addr
+
 ; save the game to replace a bad save
 .save_game
     ; Set up vanilla SRAM buffer.
@@ -73,9 +89,9 @@ clear_saved_checkpoints:
     ; Save to SRAM/BW-RAM.
     jsl $009BC9|!bank
 
-; play sound effect
-.play_sfx
-    lda #$0F : sta $1DF9|!addr
+; play save sound effect
+.play_save_sfx
+    lda #$22 : sta $1DFC|!addr
 
 .return
     rtl
