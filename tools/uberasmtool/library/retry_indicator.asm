202d566c80cd4338d97247b7dab58aee70c06ed1
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-12-17 17:54:34 -0330

    Update retry indicator

--- tools/uberasmtool/library/retry_indicator.asm
+++ tools/uberasmtool/library/retry_indicator.asm
@@ -1,11 +1,20 @@
 incsrc "../retry_config/ram.asm"
 
+; The retry arrows graphic is stored in GFX29
+!Tile = $F6
+
+; To change the location check the diagram here:
+; https://www.smwcentral.net/?p=memorymap&a=detail&game=smw&region=ram&detail=a70ec5339321
+!Location = $0EFE|!addr
+
 main:
+    ; check the retry ram prompt type
     lda !ram_prompt_override
-    cmp #$02 : beq draw
-    cmp #$03 : beq draw
+    cmp #$01 : beq draw     ; if prompt + jingle
+    cmp #$02 : beq draw     ; if prompt + sfx
+    cmp #$03 : beq draw     ; if instant
     rtl
 
 draw:
-    lda #$F6 : sta $0F19|!addr
+    lda #!Tile : sta !Location
     rtl
