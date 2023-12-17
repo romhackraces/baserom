6eb6a71e6a7c09ca8d6890837bac58fcc4aea6f5
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2023-12-17 18:35:56 -0330

    Update retry_indicator.asm

--- tools/uberasmtool/library/retry_indicator.asm
+++ tools/uberasmtool/library/retry_indicator.asm
@@ -1,20 +1,26 @@
 incsrc "../retry_config/ram.asm"
 
-; The retry arrows graphic is stored in GFX29
+; Tile number to display; the baserom's retry arrows graphic is in GFX29
 !Tile = $F6
 
-; To change the location check the diagram here:
-; https://www.smwcentral.net/?p=memorymap&a=detail&game=smw&region=ram&detail=a70ec5339321
+; Important: the YXPCCCTT (orientation, palette, etc.) values are hardcoded in ROM.
+; If you want to change those you will have to hex edit the value for each status bar tile.
+; See here: https://www.smwcentral.net/?p=memorymap&a=detail&game=smw&region=ram&detail=f2a5bf492c3e
+
+; Where on the status bar to show the tile
 !Location = $0EFE|!addr
 
+; This location is a single 8x8 tile, to change the location check the diagram here:
+; https://www.smwcentral.net/?p=memorymap&a=detail&game=smw&region=ram&detail=a70ec5339321
+
 main:
-    ; check the retry ram prompt type
+    ; check the retry ram for the prompt type
     lda !ram_prompt_override
-    cmp #$01 : beq draw     ; if prompt + jingle
-    cmp #$02 : beq draw     ; if prompt + sfx
-    cmp #$03 : beq draw     ; if instant
+    cmp #$01 : beq draw_tile     ; if prompt + jingle
+    cmp #$02 : beq draw_tile     ; if prompt + sfx
+    cmp #$03 : beq draw_tile     ; if instant
     rtl
 
-draw:
+draw_tile:
     lda #!Tile : sta !Location
     rtl
