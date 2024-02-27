74c0620b8afd9749e7e6115acdfd74abdf9345a8
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2024-02-27 15:40:03 -0330

    Apply fix to line act as patch
    - fix taken from: https://smwc.me/s/23847/76456

--- resources/patches/fixes/line_guide_actas_fix.asm
+++ resources/patches/fixes/line_guide_actas_fix.asm
@@ -13,10 +13,10 @@
 ; worldpeace's note:
 ; The vanilla code of line guide interaction sometimes tries to restore the sprite's position from backup RAM, even if they aren't properly stored.
 ; This results in teleportation of line-guided sprites. (Like other vanilla glitches, this has been regarded as a feature and utilized for vanilla tricks!)
-; 
+;
 ; The previous version assumed the correctness of Nintendo's code however, and accessed some invalid "acts like" values as a result.
 ; It caused game freezing in some emulators (e.g. higan v092); for those who're interested, the patch got the map16 value outside of the level area and repeated a loop infinitely.
-; 
+;
 ; My version fixes the freezing issue, as well as giving some extra option regarding behaviors of line guide.
 
 
@@ -28,9 +28,9 @@
 ; Explanation: you can choose the frequency of odd behaviors, which are the following:
 ; 1) Map16 tiles in the page 1 (or acting like one) work as "line guide end".
 ; 2) Those tiles let line-guided sprites teleport to certain position. (see ft029's swissotel in vldcx)
-; 
+;
 ; These behaviors can be rarely observed in vanilla; only when the line-guided sprite is at a specific position which is decided by the map16 page of interacting tiles.
-; 
+;
 ; $03 = always allows those two behaviors regardless of the position of a line-guided sprite, like the previous version of the patch.
 ; $02 = always allows the "line guide end" behavior, but disallows the teleportation. (good for non-janky hacks)
 ; $01 = sparsely allows them as frequent as the true vanilla where there are only map16 pages 0 and 1. (might be useful for a restrictive compilation hack of something)
@@ -69,15 +69,20 @@ freecode
 LineGuideActFix:
 	CMP #$80
 	BCS .reject
--
-	XBA
+	PHX
+	LDX #$00
+	CMP #$40
+	BCC +
+	LDX #$16
++
+-	XBA
 	LDA $1693|!addr
 	REP #$20
 	ASL
-	ADC $06F624|!bank
+	ADC $06F624|!bank,x
 	STA $0D
 	SEP #$20
-	LDA $06F626|!bank
+	LDA $06F626|!bank,x
 	STA $0F
 	REP #$20
 	LDA [$0D]
@@ -86,6 +91,7 @@ LineGuideActFix:
 	XBA
 	CMP #$02
 	BCS -
+	PLX
 
 	if !glitch_level == $00
 		CMP #$00
