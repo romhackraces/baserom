911bff8e716d52c15493106e3d99f1c2a17c954c
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2024-02-28 16:48:49 -0330

    Update to AMK 1.0.10

--- tools/init/tool_defines.ps1
+++ tools/init/tool_defines.ps1
@@ -4,7 +4,7 @@
 
 # AddmusicK
 $AddMusicK_Dir = "$ToolsDir\addmusick\"
-$AddMusicK_Download = "https://dl.smwcentral.net/31558/AddmusicK%201.0.9.zip"
+$AddMusicK_Download = "https://dl.smwcentral.net/35333/"
 $AddMusicK_Junk = @("src.zip", "addmusicMRemover.pl", "Makefile", "asar.exe")
 $AddMusicK_Docs = @("readme_files", "readme.html")
 
@@ -16,19 +16,19 @@ $Callisto_Docs = @("documentation")
 
 # Flips
 $Flips_Dir = "$ToolsDir\flips\"
-$Flips_Download = "https://dl.smwcentral.net/11474/floating.zip"
+$Flips_Download = "https://dl.smwcentral.net/11474/"
 $Flips_Junk = @("license.txt", "flips-linux", "boring.zip", "src.zip")
 $Flips_Docs = @("")
 
 # GPS
 $GPS_Dir = "$ToolsDir\gps\"
-$GPS_Download = "https://dl.smwcentral.net/31515/GPS%20%28V1.4.4%29.zip"
+$GPS_Download = "https://dl.smwcentral.net/31515/"
 $GPS_Junk = @("src.zip", "Changes.txt")
 $GPS_Docs = @("README.txt")
 
 # Lunar Magic
 $LunarMagic_Dir = "$ToolsDir\lunar_magic\"
-$LunarMagic_Download = "https://dl.smwcentral.net/35121/lm340.zip"
+$LunarMagic_Download = "https://dl.smwcentral.net/35121/"
 $LunarMagic_Junk = "readme.txt"
 $LunarMagic_Docs = @("")
 
