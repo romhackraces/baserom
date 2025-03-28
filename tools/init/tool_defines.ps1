870953f766d42b3fc920103e872af2863ea4da59
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2025-03-28 19:37:43 -0230

    update callisto to 0.4.2

--- tools/init/tool_defines.ps1
+++ tools/init/tool_defines.ps1
@@ -10,7 +10,7 @@ $AddMusicK_Docs = @("readme_files", "readme.html")
 
 # Callisto
 $Callisto_Dir = "$WorkingDir\buildtool\"
-$Callisto_Download = "https://github.com/Underrout/callisto/releases/download/v0.4.1/callisto-v0.4.1.zip"
+$Callisto_Download = "https://github.com/Underrout/callisto/releases/download/v0.4.2/callisto-v0.4.2.zip"
 $Callisto_Junk = @("ASAR_LICENSE", "LICENSE", "config", "asar", "initial_patches")
 $Callisto_Docs = @("documentation")
 
