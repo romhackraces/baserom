d3aff43da786589a77e3cb3224daa0eeaa7752d1
AmperSam <81581391+AmperSam-git@users.noreply.github.com>
2025-07-08 23:39:40 -0230

    Fix the poison mushroom so it doens't disappear

--- resources/patches/enhancements/poison_mushroom.asm
+++ resources/patches/enhancements/poison_mushroom.asm
@@ -1,33 +1,46 @@
-; Poison mushroom patch
-; original binary by mikey, asm version plus slight modification by JackTheSpades
+;=======================================================================
+; Poison Mushroom by mikeyk
+; modifications by JackTheSpades and AmperSam
+;=======================================================================
+
+;Applying this patch will specify that an existing sprite number will be a
+;Poison Mushroom instead (ideally an unused one). Available unused sprite
+;numbers: 12, 69, 85 (Default), 96.
 ;
-; This patch will overwrite an existing sprite and turn it into the poison mushroom instead.
-; Note that you shouldn't reapply this patch to the same ROM after changing !NUM as it will
-; NOT remove the previous insertion.
+;Don't reapply this patch to the same ROM after specifying a sprite
+;number as it will NOT remove the previous insertion.
+;
+;Setting the Extra Bit of the sprite in Lunar Magic will make this
+;sprite a kill variant Mushroom. This requires PIXI to be applied
+;to your project.
 
-; You can however freely change the other defines and reapply then
 
-!NUM ?= $85         ;NORMAL sprite number to insert as
-!Tile = $E6         ;tile to use gfx
-!PalHurt = $A       ;palette to use (not yxppccct, just plain normal palette row)
-!PalKill = $B       ;palette to use for the kill version
-!GfxPage = 0		;use second graphics page 0=no,1=yes
+;=======================================================================
+; DEFINES
+;=======================================================================
+
+!NUM ?= $85         ; NORMAL sprite number to insert as
+!Tile = $E6         ; sprite tile to use for graphics
+!PalHurt = $A       ; palette row number to use (not YXPPCCCT)
+!PalKill = $B       ; palette row for the kill version
+!GFXPage = 0		; 0 = first graphics page, 1 = second page
+
+
+;=======================================================================
+; CODE
+;=======================================================================
 
 assert !NUM < $C9
 
-if read1($00FFD6) == $15
-    sfxrom
-    !dp = $6000
-    !addr = !dp
-    !bank = $000000
-    !gsu = 1
-elseif read1($00FFD5) == $23
+if read1($00FFD5) == $23
     sa1rom
     !dp = $3000
     !addr = $6000
     !bank = $000000
     !extra_bits = $400040
     !sa1 = 1
+    !14C8 = $3242
+    !9E = $3200
 else
     lorom
     !dp = $0000
@@ -35,7 +48,8 @@ else
     !bank = $800000
     !extra_bits = $7FAB10
     !sa1 = 0
-    !gsu = 0
+    !14C8 = $14C8
+    !9E = $9E
 endif
 
 org ($01817D+(!NUM*2))|!bank    ;sprite init pointer
@@ -48,7 +62,7 @@ org ($07F26C+!NUM)|!bank        ;tweaker 1656
 org ($07F335+!NUM)|!bank        ;tweaker 1662
     db $00
 org ($07F3FE+!NUM)|!bank        ;tweaker 166E
-    db (!PalHurt-8)<<1|!GfxPage
+    db (!PalHurt-8)<<1|!GFXPage
 org ($07F4C7+!NUM)|!bank        ;tweaker 167A
     db $C2
 org ($07F590+!NUM)|!bank        ;tweaker 1686
@@ -56,42 +70,50 @@ org ($07F590+!NUM)|!bank        ;tweaker 1686
 org ($07F659+!NUM)|!bank        ;tweaker 190F
     db $40
 
-;handle power up routine
-org $01C4CB|!bank
-    autoclean JML PoisonHurt
+; hijack power up routine
+org $01C4C6|!bank
+    autoclean JML PoisonMushroom
 
-;handle GFX routine
+; handle GFX routine
 org $01C6D6|!bank
-    autoclean JSL PoisonGFX
+    autoclean JSL PoisonMushroomGFX
 
 freecode
 
-;input:  A = sprite number
-PoisonHurt:                 ; code JML's here
-    CMP #$21                ;\
-    BNE +                   ;| retore code for BEQ
-    JML $01C4CF|!bank       ;/
-+   CMP #!NUM               ;\ check if sprite is poison mushoroom
-    BNE .return             ;/
+PoisonMushroom:
+    LDA !9E,x				;\
+    CMP #$21                ;| check if moving coin
+    BNE .not_coin           ;/
 
-    LDA !extra_bits,x       ;\ check extra bits
+    JML $01C4CF|!bank       ; jump to give coin routine
+
+.not_coin
+    CMP #!NUM               ;\ check if sprite is poison mushroom
+    BNE .erase              ;/ ..otherwise erase sprite
+
+    LDA !extra_bits,x       ;\ check extra bits for kill variant
     AND #$04                ;|
     BNE .kill               ;/
 
-.hurt
-    JSL $00F5B7|!bank       ;\ hurt player
-    BRA .rts                  ;/
+    JSL $00F5B7|!bank       ; run hurt routine
+    BRA .rts
 
 .kill
-    JSL $00F606|!bank       ;kill player
+    JSL $00F606|!bank       ; run kill routine
+
 .rts
-    JML $01C57F|!bank       ;jump-returns to RTS
+    JML $01C57F|!bank       ; jump to end of routine RTS
+
+.erase
+    STZ !14C8,x             ; erase sprite if not poison mushroom
+
 .return
-    JML $01C538|!bank        ;return to normal power-up code
+    JML $01C538|!bank       ; return to normal power-up code
+
 
 ;input:  A = sprite number
 ;output: A = tile number
-PoisonGFX:                  ; code JSL's here
+PoisonMushroomGFX:
     CMP.b #!NUM-$74         ;\ check if sprite is poison mushroom and skip
     BNE +                   ;/ if not.
 
