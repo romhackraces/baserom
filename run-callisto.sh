48f8e570fee3a3c819d54fd94fbc403886e43115
Jeanine Adkisson <jeanine.adkisson@gmail.com>
2026-01-11 14:45:23 -0500

    Add build scripts and instructions for non-windows setup (#168)
    * add build scripts and instructions for non-windows setup
    
    * check for wine dependency and warn in case of interprocess bug
    
    * add further sanity checks to the bash scripts
    
    * error if bash version is too old
    
    * use -ef to make sure we are in the correct repository. the script runs rm, so it's good to triple check
    
    * add more setup instructions and purge windows line endings before patching
    
    ---------
    
    Co-authored-by: jneen <jneen@jneen.net>

---
+++ run-callisto.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+. "$(dirname "$0")"/setup/common.sh || exit 1
+
+check-dependencies wine || exit $?
+
+case $(wine --version) in
+  wine-9.*|wine-10.*)
+    msg-fail "WARNING: Lunar Magic reloading will not work in wine <= 10. Please upgrade to wine 11."
+esac
+
+export WINEDEBUG=-all
+
+callisto() {
+  exec wine tools/Callisto/callisto.exe "$@"
+}
+
+rm -rf workspace/temp
+mkdir -p workspace/temp
+cp -r workspace/Graphics workspace/temp/Graphics
+cp -r workspace/ExGraphics workspace/temp/ExGraphics
+
+if [[ $# == 0 ]]; then
+  callisto update
+else
+  callisto "$@"
+fi
